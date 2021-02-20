59991934f372c8ff0b2520df5f310302a4ff27fd
David Henry <dah06a@acu.edu>
2021-02-20 13:00:24 -0600

    Completed final assignment task 3.

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -3,6 +3,7 @@ import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'r
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { Fade, Stagger } from 'react-animation-components';
 
 function RenderPartner({partner}) {
     if (partner) {
@@ -31,16 +32,21 @@ function PartnerList(props) {
 
     const partners = props.partners.partners.map(partner => {
         return (
-            <div className="col mt-4">
-                <Media list tag="li" key={partner.id}>
-                    <RenderPartner partner={partner} />
-                </Media>
-            </div>
-
+            <Fade key={partner.id}>
+                <div className="col mt-4">
+                    <Media list tag="li">
+                        <RenderPartner partner={partner} />
+                    </Media>
+                </div>
+            </Fade>
         );
     });
 
-    return partners;
+    return (
+        <Stagger in>
+            {partners}
+        </Stagger>
+    );
 
 }
 
